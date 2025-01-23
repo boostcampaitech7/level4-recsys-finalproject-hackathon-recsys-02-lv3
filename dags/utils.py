c33df000fc4e84aecf6dbd825311821dd32eebb4
jaypark <jwpark3639@gmail.com>
2025-01-24 01:03:04 +0900

    [KAN-77] feat: create file to define constants

---
+++ dags/utils.py
@@ -0,0 +1,20 @@
+from airflow.models.variable import Variable
+
+import os
+
+class Config:
+    RDB_NAME = Variable.get("RDB_NAME")
+    VECTOR_DB_NAME = Variable.get("VECTOR_DB_NAME")
+    DB_USER = Variable.get("DB_USER")
+    DB_PASSWORD = Variable.get("DB_PASSWORD")
+    DB_HOST = Variable.get("DB_HOST")
+    DB_PORT = Variable.get("DB_PORT")
+
+    REGIONS = Variable.get("REGIONS", deserialize_json=True)
+
+
+
+class Directory:
+    AIRFLOW_HOME = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    DOWNLOADS_DIR = os.path.join(AIRFLOW_HOME, 'download')
+    TRANSFORM_DIR = os.path.join(AIRFLOW_HOME, 'transform')
