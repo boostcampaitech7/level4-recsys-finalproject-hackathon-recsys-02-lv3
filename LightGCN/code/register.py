556d0bd05c3c4481967d7c98ceb41d12e8daf7fe
eun <jjjaeunn@gmail.com>
2025-01-23 14:55:06 +0900

    [KAN-10] style: 경로 포맷팅 #5

--- LightGCN/code/register.py
+++ LightGCN/code/register.py
@@ -4,10 +4,7 @@ import model
 import utils
 from pprint import pprint
 
-if world.dataset in ['gowalla', 'yelp2018', 'amazon-book', 'spotify']:
-    dataset = dataloader.Loader(path="../data/"+world.dataset)
-elif world.dataset == 'lastfm':
-    dataset = dataloader.LastFM()
+DATA = dataloader.Loader(path=world.DATA_PATH+"/spotify/")
 
 print('===========config================')
 pprint(world.config)
