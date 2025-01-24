6c8e66855e6de4d7c16808e2eb496cecf7fe8e9f
jaypark <jwpark3639@gmail.com>
2025-01-24 18:45:10 +0900

    [KAN-77] feat: Update overall contents

--- dags/daily_spotify_dag.py
+++ dags/daily_spotify_dag.py
@@ -13,11 +13,13 @@ from tqdm import tqdm
 import requests
 from bs4 import BeautifulSoup 
 import os
-import ray
 from datetime import datetime
 import pandas as pd
 import psycopg2
-from utils import Config, Directory
+import math
+import logging
+
+from dags.utils import Config, Directory
 
 def get_soup(url):
     html = requests.get(url).text
@@ -36,16 +38,18 @@ def get_last_fm_url_from_db(**context):
     query = "SELECT * FROM track;"
     df = pd.read_sql(query, conn)
     total_last_fm_url_list = df['last_fm_url'].unique().tolist()
+    print(f"the total number of url = {len(total_last_fm_url_list)}")
     context["ti"].xcom_push(key='total_last_fm_url_list', value=total_last_fm_url_list)
 
-
+# ray 사용
 def get_track_artist_list(**context):
     artist_list = []
     track_list = []
 
     country_daily_chart_url = Config.REGIONS
 
-    for url in tqdm(country_daily_chart_url):
+    print("Crawling Start!")
+    for url in tqdm(country_daily_chart_url[:2]):
         try:
             soup = get_soup(url)
             rows = soup.select('div.subcontainer>table')[0].find_all('td', class_='text mp')
@@ -69,9 +73,10 @@ def get_track_artist_list(**context):
     total_last_fm_url_list = context['ti'].xcom_pull(key="total_last_fm_url_list")
     last_fm_url_list = list(set(url_list) - set(total_last_fm_url_list))
 
+    print(f"the number of url to be searched from last fm : {len(last_fm_url_list)}")
     context["ti"].xcom_push(key='last_fm_url_list', value=last_fm_url_list)
 
-@ray.remote
+
 def get_listeners(url):
     try:
         soup = get_soup(url)
@@ -86,7 +91,7 @@ def get_listeners(url):
         listeners = '0'
     return int(listeners)
 
-@ray.remote
+
 def get_length(url):
     try:
         soup = get_soup(url)
@@ -96,7 +101,7 @@ def get_length(url):
         length = ''
     return length
 
-@ray.remote
+
 def get_genres(url):
     try:
         soup = get_soup(url)
@@ -106,7 +111,7 @@ def get_genres(url):
         genres = ''
     return genres
 
-@ray.remote
+
 def get_img_url(url):
     try:
         soup = get_soup(url)
@@ -115,7 +120,7 @@ def get_img_url(url):
         img_url = 'https://lastfm.freetls.fastly.net/i/u/300x300/c6f59c1e5e7240a4c0d427abd71f3dbb.jpg'
     return img_url
 
-@ray.remote
+
 def get_introduction(url):
     try:
         soup = get_soup(url)
@@ -124,73 +129,86 @@ def get_introduction(url):
         introduction = ''
     return introduction
 
-def get_info(**context):
-    num_cpus = 8
-    ray.init(num_cpus=num_cpus, ignore_reinit_error=True)
-    
-    last_fm_url_list = context['ti'].xcom_pull(key="last_fm_url_list")
+def get_info(num_partition: int, idx:int, **context):
+    last_fm_url_list = context['ti'].xcom_pull(key="last_fm_url_list")[:500]
     print(f"Today's the number of new tracks : {len(last_fm_url_list)}")
 
-    batch_size = 10  # CPU 코어 수를 고려한 배치 크기
-    results = []
-    
+    partition = math.ceil(len(last_fm_url_list) / num_partition)
+    start, end = idx * partition, (idx + 1) * partition
+    last_fm_url_list = last_fm_url_list[start:end]
+    print(f"start : {start}")
+    print(f"end : {end}")
+
+
     print("Start crawling")
-    for i in tqdm(range(len(last_fm_url_list), batch_size)):
+    results = []
+    batch_size = math.ceil(len(last_fm_url_list) / 20)
+    for i in tqdm(range(0, len(last_fm_url_list), batch_size)):
         batch_urls = last_fm_url_list[i:i + batch_size]
-        
-        # 배치 단위로 작업 제출
-        batch_refs = {
-            'url': batch_urls,
-            'listeners': [get_listeners.remote(url) for url in batch_urls],
-            'length': [get_length.remote(url) for url in batch_urls],
-            'genres': [get_genres.remote(url) for url in batch_urls],
-            'img_url': [get_img_url.remote(url) for url in batch_urls],
-            'introduction': [get_introduction.remote(url + '/+wiki') for url in batch_urls]
-        }
-        
-        # 배치 결과 수집
         batch_results = {
-            'url': batch_refs['url'],
-            'listeners': ray.get(batch_refs['listeners']),
-            'length': ray.get(batch_refs['length']),
-            'genres': ray.get(batch_refs['genres']),
-            'img_url': ray.get(batch_refs['img_url']),
-            'introduction': ray.get(batch_refs['introduction'])
+            'last_fm_url': batch_urls,
+            'listeners': [get_listeners(url) for url in batch_urls],
+            'length': [get_length(url) for url in batch_urls],
+            'genres': [get_genres(url) for url in batch_urls],
+            'img_url': [get_img_url(url) for url in batch_urls],
+            'introduction': [get_introduction(url + '/+wiki') for url in batch_urls]
         }
-        
-        # 배치 결과를 DataFrame으로 변환하여 저장
+
         batch_df = pd.DataFrame(batch_results)
         results.append(batch_df)
 
     print("Crawling completed!")
 
     final_df = pd.concat(results, ignore_index=True)
+    context['task_instance'].xcom_push(key=f'partition_result_{idx}', value=final_df.to_dict())
 
-    now = datetime.now()
-    date = now.strftime("%Y-%m-%d")
+def combine_results(**context):
+    """모든 파티션의 결과를 합치는 함수"""
+    all_results = []
+    
+    # 각 파티션의 결과를 수집
+    for i in range(NUM_PARTITION):
+        partition_result = context['task_instance'].xcom_pull(
+            task_ids=f'get_info_group_task.get_info_{i+1}',
+            key=f'partition_result_{i}'
+        )
+        if partition_result:
+            df = pd.DataFrame.from_dict(partition_result)
+            all_results.append(df)
+    
+    # 모든 결과를 하나의 DataFrame으로 합치기
+    final_df = pd.concat(all_results, ignore_index=True)
+    
+    # artist와 track 열 생성
+    final_df['artist'] = final_df['last_fm_url'].apply(lambda x: x.split('/')[-3].replace('+', ' '))
+    final_df['track'] = final_df['last_fm_url'].apply(lambda x: x.split('/')[-1].replace('+', ' '))
+
+    # 최종 결과 저장
+    date = datetime.now().strftime("%Y-%m-%d")
     path = os.path.join(Directory.DOWNLOADS_DIR, f"spotify/{date}.csv")
-    final_df.to_csv(path)
+    final_df.to_csv(path, index = False)
 
-
-def upload_file_to_gcs(filename: str, object_name: str, bucket_name: str, replace: bool = True) -> None:
+def upload_file_to_gcs(bucket_name: str, replace: bool = True) -> None:
     """ Airflow Hook으로 GCS에 파일을 업로드하는 메서드
     Args:
-        filename (str): 업로드할 로컬 파일 경로
-        object_name (str): GCS에 저장될 객체 이름/경로
+        source_path (str): 업로드할 로컬 파일 경로
+        destination_path (str): GCS에 저장될 객체 이름/경로
         bucket_name (str): GCS 버킷 이름
         replace (bool): 덮어쓰기 여부 (기본값: True)
     """
     hook = GCSHook(gcp_conn_id="google_cloud_default")
     
-    if not replace and hook.exists(bucket_name=bucket_name, object_name=object_name):
-        print(f"Object {object_name} already exists in bucket {bucket_name}")
+    if not replace and hook.exists(bucket_name=bucket_name, object_name=destination_path):
+        print(f"Object {destination_path} already exists in bucket {bucket_name}")
         return
-        
+
+    date = datetime.now().strftime("%Y-%m-%d")
+    source_path = os.path.join(Directory.DOWNLOADS_DIR, f"{date}.csv")
+    destination_path = f'download/spotify/{date}.csv'    
     hook.upload(
         bucket_name=bucket_name,
-        object_name=object_name,
-        filename=filename,
-        gzip=False  # 필요시 True로 설정하여 gzip 압축 가능
+        object_name=destination_path,
+        filename=source_path
     )
 
 def delete_xcoms_for_dags(dag_ids, **kwargs):
@@ -199,7 +217,7 @@ def delete_xcoms_for_dags(dag_ids, **kwargs):
             XCom.dag_id.in_(dag_ids)
         ).delete(synchronize_session=False)
         session.commit()
-        
+
 
 default_args = {
     'owner': 'airflow',
@@ -208,8 +226,9 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+NUM_PARTITION = 8
 
-with DAG('user_embedding_update_dag',
+with DAG('daily_spotify_dag',
         default_args=default_args,
         schedule='0 0 * * *',
         start_date=datetime(2024, 1, 1),
@@ -232,14 +251,39 @@ with DAG('user_embedding_update_dag',
         provide_context=True
     )
 
-    get_info_task = PythonOperator(
-        task_id='get_info_task',
-        python_callable=get_info,
-        provide_context=True
+    with TaskGroup("get_info_group_task") as get_info_group_task:
+        for i in range(NUM_PARTITION):
+            get_info_task = PythonOperator(
+                task_id=f"get_info_{i+1}",
+                python_callable=get_info,
+                op_kwargs={
+                    "num_partition": NUM_PARTITION,
+                    "idx": i
+                }
+            )
+
+    combine_results_task = PythonOperator(
+        task_id='combine_results_task',
+        python_callable=combine_results
     )
 
     upload_file_to_gcs_task = PythonOperator(
         task_id='upload_file_to_gcs_task',
         python_callable=upload_file_to_gcs,
+        op_kwargs= {
+            "bucket_name": Config.BUCKET_NAME
+        },
         provide_context=True
     )
+
+    delete_xcom_task = PythonOperator(
+            task_id="delete_xcom_task",
+            python_callable=delete_xcoms_for_dags,
+            op_kwargs={'dag_ids': ['get_last_fm_url_from_db_task', 'get_track_artist_list_task', 'get_info_group_task']}
+        )
+    
+    end_task = EmptyOperator(
+        task_id="end_task"
+    )
+    
+    start_task >> get_last_fm_url_from_db_task >> get_track_artist_list_task >> get_info_group_task >> combine_results_task >>  upload_file_to_gcs_task >> delete_xcom_task >> end_task
