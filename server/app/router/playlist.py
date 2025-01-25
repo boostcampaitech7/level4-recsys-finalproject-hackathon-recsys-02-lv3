f501e48b7ba54fed7a29efb5d5b24ca838e07b85
jinnk0 <jin8073@gmail.com>
2025-01-25 05:45:37 +0000

    [KAN-76] feat: update existing features to use token refresh logic (#14)

--- server/app/router/playlist.py
+++ server/app/router/playlist.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, HTTPException, Query, Depends
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from app.config.settings import Settings
+from app.utils.spotify_api_service import SpotifyApiService
 from app.dto.playlist import Playlist, Artist, Track, InsertTrackRequest
 from db.database_postgres import PostgresSessionLocal, User
 import logging
@@ -17,31 +18,28 @@ def get_db():
 router = APIRouter()
 setting = Settings()
 logger = logging.getLogger("uvicorn")
+spotify_service = SpotifyApiService()
 
 @router.get("/users/{user_id}/playlists", response_model=list[Playlist])
 async def get_user_playlists(user_id: int, db: Session = Depends(get_db)):
     find_user = db.query(User).filter(User.user_id == user_id).first()
     if not find_user:
         raise HTTPException(status_code=404, detail="cannot find user")
+    
+    response = await spotify_service._make_request(
+        method='GET',
+        url=f"{setting.SPOTIFY_API_URL}/users/{find_user.spotify_id}/playlists",
+        user=find_user,
+        db=db
+    )
 
-    async with httpx.AsyncClient() as client:
-        response = await client.get(
-            f"{setting.SPOTIFY_API_URL}/users/{find_user.spotify_id}/playlists",
-            headers={
-                "Authorization":f"Bearer {find_user.access_token}"
-            }
-        )
-
-        if response.status_code == 200:
-            items = response.json()["items"]
-            playlists = [Playlist(
-                playlist_id=item["id"], 
-                playlist_name=item["name"],
-                playlist_img_url=item["images"][0]["url"] if item["images"] else None
-            ).dict()for item in items]
-            return JSONResponse(status_code=200, content={"message":"playlists loaded successfully", "items":playlists})
-        else:
-            raise HTTPException(status_code=response.status_code, detail=response.json())
+    items = response["items"]
+    playlists = [Playlist(
+        playlist_id=item["id"], 
+        playlist_name=item["name"],
+        playlist_img_url=item["images"][0]["url"] if item["images"] else None
+    ).dict()for item in items]
+    return JSONResponse(status_code=200, content={"message":"playlists loaded successfully", "items":playlists})
 
 @router.get("/test/playlists/{playlist_id}", response_model=list[Track])
 async def get_playlist_track(playlist_id: str, user_id: int = Query(...), \
@@ -49,24 +47,22 @@ async def get_playlist_track(playlist_id: str, user_id: int = Query(...), \
     find_user = db.query(User).filter(User.user_id == user_id).first()
     if not find_user:
         raise HTTPException(status_code=404, detail="cannot find user")
-    async with httpx.AsyncClient() as client:
-        response = await client.get(
-            f"{setting.SPOTIFY_API_URL}/playlists/{playlist_id}/tracks",
-            headers={
-                "Authorization":f"Bearer {find_user.access_token}"
-            }
-        )
-        if response.status_code == 200:
-            items = response.json()["items"]
-            tracks = [Track(
-                track_id=item["track"]["id"], 
-                track_name=item["track"]["name"],
-                track_img_url=item["track"]["album"]["images"][0]["url"] if item["track"]["album"]["images"] else None,
-                artists=[Artist(artist_name=artist["name"]).dict() for artist in item["track"]["artists"]]
-            ).dict() for item in items]
-            return JSONResponse(status_code=200, content={"message":"playlists loaded successfully", "items":tracks})
-        else:
-            raise HTTPException(status_code=response.status_code, detail=response.json())
+    
+    response = await spotify_service._make_request(
+        method='GET',
+        url=f"{setting.SPOTIFY_API_URL}/playlists/{playlist_id}/tracks",
+        user=find_user,
+        db=db
+    )
+
+    items = response["items"]
+    tracks = [Track(
+        track_id=item["track"]["id"], 
+        track_name=item["track"]["name"],
+        track_img_url=item["track"]["album"]["images"][0]["url"] if item["track"]["album"]["images"] else None,
+        artists=[Artist(artist_name=artist["name"]).dict() for artist in item["track"]["artists"]]
+    ).dict() for item in items]
+    return JSONResponse(status_code=200, content={"message":"playlists loaded successfully", "items":tracks})
         
 @router.post("/playlists/{playlist_id}/tracks")
 async def insert_playlist_track(playlist_id: str, tracks: InsertTrackRequest, user_id: int = Query(...), \
@@ -75,33 +71,29 @@ async def insert_playlist_track(playlist_id: str, tracks: InsertTrackRequest, us
     if not find_user:
         raise HTTPException(status_code=404, detail="cannot find user")
     
-    headers = {"Authorization":f"Bearer {find_user.access_token}"}
-    async with httpx.AsyncClient() as client:
-        track_uris = []
-        for track in tracks.items:
-            logger.info(f"{tracks.items}")
-            query = f'track:"{track.track_name}" artist:"{track.artists[0].artist_name}"'
-            response = await client.get(
-                f"{setting.SPOTIFY_API_URL}/search",
-                headers=headers,
-                params={"q":query, "type":"track", "limit":3}
-            )
-            if response.status_code == 200:
-                items = response.json()['tracks']['items']
-                if items:
-                    track_uris.append(items[0]['uri'])
-            else:
-                raise HTTPException(status_code=response.status_code, detail=response.json())
+    track_uris = []
+    for track in tracks.items:
+        query = f'track:"{track.track_name}" artist:"{track.artists[0].artist_name}"'
+        response = await spotify_service._make_request(
+            method='GET',
+            url=f"{setting.SPOTIFY_API_URL}/search",
+            user=find_user,
+            db=db,
+            params={"q":query, "type":"track", "limit":3}
+        )
+        items = response['tracks']['items']
+        if items:
+            track_uris.append(items[0]['uri'])
+            
         
-        if track_uris:
-            response = await client.post(
-                f"{setting.SPOTIFY_API_URL}/playlists/{playlist_id}/tracks",
-                headers=headers,
-                json={"uris":track_uris}
-            )
-            if response.status_code == 200:
-                return JSONResponse(status_code=200, content={"message":"Tracks added successfully"})
-            else:
-                raise HTTPException(status_code=response.status_code, detail=response.json())
-        else:
-            return JSONResponse(status_code=200, content={"message":"cannot find tracks"})
+    if track_uris:
+        response = await spotify_service._make_request(
+            method='POST',
+            url=f"{setting.SPOTIFY_API_URL}/playlists/{playlist_id}/tracks",
+            user=find_user,
+            db=db,
+            json={"uris":track_uris}
+        )
+        return JSONResponse(status_code=200, content={"message":"Tracks added successfully"})
+    else:
+        return JSONResponse(status_code=200, content={"message":"cannot find tracks"})
--- server/app/router/recommendation.py
+++ server/app/router/recommendation.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from app.dto.recommendation import OnboardingRequest, PlaylistRecommendation, TrackIdPair
+from app.utils.spotify_api_service import SpotifyApiService
 from app.config.settings import Settings
 from app.dto.playlist import Artist, Track
 from db.database_postgres import PostgresSessionLocal, User
@@ -20,6 +21,7 @@ def get_db():
 logger = logging.getLogger("uvicorn")
 router = APIRouter()
 setting = Settings()
+spotify_service = SpotifyApiService()
 
 @router.post("/onboarding")
 async def get_onboarding(onboadingRequest: OnboardingRequest):
@@ -76,37 +78,59 @@ async def get_recommendation_by_playlists(playlist_id: str, user_id: int = Query
     find_user = db.query(User).filter(User.user_id == user_id).first()
     if not find_user:
         raise HTTPException(status_code=404, detail="cannot find user")
+    
+    response = await spotify_service._make_request(
+        method='GET',
+        url=f"{setting.SPOTIFY_API_URL}/playlists/{playlist_id}/tracks",
+        user=find_user,
+        db=db
+    )
+    items = response["items"]
+    tracks = []
+    for item in items:
+        query = text("""
+            SELECT t.track_id
+            FROM track t
+            JOIN track_artist ta ON ta.track_id = t.track_id
+            JOIN artist a ON ta.artist_id = a.artist_id
+            WHERE LOWER(REPLACE(a.artist, ' ', '')) = LOWER(REPLACE(:artist_name, ' ', ''))
+            AND LOWER(REPLACE(t.track, ' ', '')) = LOWER(REPLACE(:track_name, ' ', ''));
+        """)
+        artist_name = item["track"]["artists"][0]["name"].split("&")[0]
+        result = db.execute(query, {"artist_name": artist_name, "track_name": item["track"]["name"]}).fetchone()
+        if result:
+            tracks.append(result[0])
+
     async with httpx.AsyncClient() as client:
-        response = await client.get(
-            f"{setting.SPOTIFY_API_URL}/playlists/{playlist_id}/tracks",
-            headers={
-                "Authorization":f"Bearer {find_user.access_token}"
-            }
+        response = await client.post(
+            f"{setting.MODEL_API_URL}/playlist",
+            json=PlaylistRecommendation(user_id=user_id, items=tracks).dict()
         )
         if response.status_code == 200:
-            items = response.json()["items"]
-            tracks = []
-            for item in items:
+            query = text("""
+                    SELECT 
+                        t.track_id,
+                        t.track AS track_name, 
+                        STRING_AGG(a.artist, ' & ' ORDER BY a.artist) AS artist_names,
+                        t.img_url
+                    FROM track t
+                    JOIN track_artist ta ON ta.track_id = t.track_id
+                    JOIN artist a ON ta.artist_id = a.artist_id
+                    WHERE t.track_id = ANY(:track_id)
+                    GROUP BY t.track, t.img_url, t.track_id
+                    ORDER BY ARRAY_POSITION(:track_id, t.track_id);
+                    """)
+            recommendations = []
+            results = db.execute(query, {"track_id": response.json()['items']}).fetchall() 
+            for result in results:
+                recommendations.append(Track(
+                    track_id=result[0],
+                    track_name=result[1],
+                    artists=[Artist(artist_name=artist).dict() for artist in result[2].split(' & ')],
+                    track_img_url=result[3],
+                ).dict())
+            for track in response.json()['items']:
                 query = text("""
-                SELECT t.track_id
-                FROM track t
-                JOIN track_artist ta ON ta.track_id = t.track_id
-                JOIN artist a ON ta.artist_id = a.artist_id
-                WHERE LOWER(REPLACE(a.artist, ' ', '')) = LOWER(REPLACE(:artist_name, ' ', ''))
-                AND LOWER(REPLACE(t.track, ' ', '')) = LOWER(REPLACE(:track_name, ' ', ''));
-                """)
-                artist_name = item["track"]["artists"][0]["name"].split("&")[0]
-                result = db.execute(query, {"artist_name": artist_name, "track_name": item["track"]["name"]}).fetchone()
-                if result:
-                    tracks.append(result[0])
-            response = await client.post(
-                f"{setting.MODEL_API_URL}/playlist",
-                json=PlaylistRecommendation(user_id=user_id, items=tracks).dict()
-            )
-            if response.status_code == 200:
-                recommendations = []
-                for track in response.json()['items']:
-                    query = text("""
                     SELECT t.track, 
                     STRING_AGG(a.artist, ' & ' ORDER BY a.artist) AS artist_names,
                     t.img_url
@@ -115,17 +139,15 @@ async def get_recommendation_by_playlists(playlist_id: str, user_id: int = Query
                     JOIN artist a ON ta.artist_id = a.artist_id
                     WHERE t.track_id = :track_id
                     GROUP BY t.track_id, t.track, t.img_url;
-                    """)
-                    result = db.execute(query, {"track_id": track}).fetchone()
-                    recommendations.append(Track(
-                        track_id=track, 
-                        track_name=result[0],
-                        artists=[Artist(artist_name=result[1]).dict()],
-                        track_img_url=result[2],
-                    ).dict())
-                return JSONResponse(status_code=200, content=recommendations)
-            else:
-                raise HTTPException(status_code=response.status_code, detail=response.json())
+                """)
+                result = db.execute(query, {"track_id": track}).fetchone()
+                recommendations.append(Track(
+                    track_id=track, 
+                    track_name=result[0],
+                    artists=[Artist(artist_name=result[1]).dict()],
+                    track_img_url=result[2],
+                ).dict())
+            return JSONResponse(status_code=200, content=recommendations)
         else:
             raise HTTPException(status_code=response.status_code, detail=response.json())
         
