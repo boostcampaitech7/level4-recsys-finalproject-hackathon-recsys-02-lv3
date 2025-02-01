2cf493366e3b3094cb43ac2416dcac9558e2b451
jjjaeunn <jjjaeunn@gmail.com>
2025-02-01 22:38:10 +0900

    feat: 라우터, API 연결 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,12 @@
-import { Outlet, createBrowserRouter } from "react-router-dom";
+import { Outlet, RouteObject, createBrowserRouter } from "react-router-dom";
 import { Layout } from "./Layout";
 
-export const ROUTES = [
+export const ROUTES: RouteObject[] = [
   { path: "", lazy: () => import("~/pages/landing") },
   { path: "authorized", lazy: () => import("~/pages/authorized") },
   { path: "onboarding", lazy: () => import("~/pages/onboarding") },
   { path: "home", lazy: () => import("~/pages/home") },
+  { path: "ocr", lazy: () => import("~/pages/ocr") },
   { path: "playlist/:playlistId", lazy: () => import("~/pages/playlist") },
 ];
 
