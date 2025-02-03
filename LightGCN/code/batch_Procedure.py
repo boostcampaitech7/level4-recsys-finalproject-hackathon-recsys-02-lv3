f98350b291aa21cc21697db8c8c3b916b1167e66
eun <jjjaeunn@gmail.com>
2025-02-03 15:52:31 +0900

    [KAN-91] feat: KNN 기반 유저 초기 임베딩 생성 메커니즘 #21

--- LightGCN/code/batch_Procedure.py
+++ LightGCN/code/batch_Procedure.py
@@ -1,49 +1,61 @@
-def train_joint(model, dataset, old_embeddings, opt_lgcn, old_knowledge, epoch, ww=None):
-    Meta_model=model
-    Meta_model.train()
+# 1. MongoDB 연결해 uid, pos, neg fetch
+# 2. KNN Logic ~.~
+# 3. fetch했던 uid / user_emb 반환
+from omegaconf import OmegaConf
+from pymongo import MongoClient
+import pandas as pd
+import os
 
-    S, sam_time = utils.UniformSample_handle( dataset, world.sample_mode)
-    print(f"BPR[lgcn handle sample time][{sam_time:.2f}]")
-    users = torch.Tensor(S[:, 0]).long()
-    posItems = torch.Tensor(S[:, 1]).long()
-    negItems = torch.Tensor(S[:, 2]).long()
-    users = users.to(world.device)
-    posItems = posItems.to(world.device)
-    negItems = negItems.to(world.device)
-    users, posItems, negItems = utils.shuffle(users, posItems, negItems)
-    total_batch = len(users) // world.config['bpr_batch_size'] + 1
-    aver_loss = 0.
-    aver_loss1 = 0.
-    aver_loss2 = 0.
-    aver_icl_regloss = 0.
+import torch
 
-    (old_User, old_Item) = old_knowledge
-    del old_User
+from inference import load_data_model, get_item_node, get_all_user_node
 
-    for (batch_i,(batch_users, batch_pos, batch_neg)) in enumerate(utils.minibatch(users, posItems, negItems, batch_size=world.config['bpr_batch_size'])):
+def get_inter_data():
+    username = "jaypark"
+    password = 'IOWABB9HnR0ew7Bf'  # 특수문자가 있는 경우 인코딩
+    uri = f"mongodb+srv://{username}:{password}@cluster0.octqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    client = MongoClient(uri)
+    
+    interaction = client['spotify']['interaction']
 
-        knn_0 = old_Item[batch_pos.long()] @ old_Item.t() 
-        knn_1 = old_Item[batch_pos.long()].pow(2).sum(dim=1).unsqueeze(1)
-        knn_2 = old_Item.pow(2).sum(dim=1).unsqueeze(0)
-        knn = knn_1 + knn_2 -2 * knn_0
-        knn = knn.pow(2)
-        knn[:,dataset.active_item_now]=np.inf
-        batch_mtach_items_itself = torch.topk(knn, world.icl_k+1, largest=False, dim=1)[1]
-        batch_mtach_items = batch_mtach_items_itself[:,1:world.icl_k+1]
+    pipeline = [{
+        "$group": {
+            "_id": "$user_id",
+            "records": {"$push": "$$ROOT"}
+        }
+    }]
 
-        del knn_0, knn_1, knn_2, knn 
+    grouped_results = list(interaction.aggregate(pipeline))
+    gcn_id = pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'gcn_track_id.csv'))
+    batch = []
 
-        loss, loss1, loss2, icl_regloss = Meta_model.get_our_loss(old_embeddings, batch_users, batch_pos, batch_neg, batch_mtach_items)
-        opt_lgcn.zero_grad()
-        loss.backward()
-        opt_lgcn.step()
-        aver_loss+=loss.cpu().item()
-        aver_loss1+=loss1.cpu().item()
-        aver_loss2+=loss2.cpu().item()
-        aver_icl_regloss+=icl_regloss.cpu().item()
+    for group in grouped_results:
+        uid, batch_pos, batch_neg = group['_id'], [], []
+        for r in group['records']:
+            try:
+                track_id = int(gcn_id.loc[gcn_id['track_id']==r['track_id'], 'gcn_track_id'])
+            except: continue
+            if r['action'] == 'positive': batch_pos.append(track_id)
+        batch.append({"uid":uid, "batch_pos": batch_pos})
+    
+    return batch
 
-    aver_loss = aver_loss / total_batch
-    aver_loss1 = aver_loss1 / total_batch
-    aver_loss2 = aver_loss2 / total_batch
-    aver_icl_regloss = aver_icl_regloss / total_batch
-    return f"[Train aver loss{aver_loss:.4e} = train_loss {aver_loss1:.4e} + icl_loss  {aver_loss2:.4e} + icl_regloss  {aver_icl_regloss:.4e} + reg]"
+@load_data_model
+def generate_temporary_user(dataset, model, batch_data, alpha=0.2) -> list:
+    e_users = get_all_user_node(dataset, model)
+    for batch_user in batch_data:
+        user_emb = torch.mean(get_item_node(model, torch.tensor(batch_user['batch_pos'], dtype=torch.long)), dim=0)
+        sim = torch.matmul(e_users, user_emb) / \
+            (torch.norm(e_users, dim=1) * torch.norm(user_emb))
+        sim_user = torch.topk(sim, k=1, largest=True)[1]
+        batch_user['u_emb'] = alpha * user_emb + (1-alpha) * e_users.detach()[sim_user].squeeze()
+        batch_user.pop('batch_pos', None)
+    return batch_data
+
+if __name__ == '__main__':
+    batch_data = get_inter_data()
+    config = OmegaConf.load('config.yaml')
+    ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
+
+    ### 전일에 적재된 유저들에 대한 임시 임베딩 생성 ###
+    print(generate_temporary_user(config, ROOT_PATH, batch_data))
--- LightGCN/code/inference.py
+++ LightGCN/code/inference.py
@@ -4,28 +4,45 @@ from omegaconf import OmegaConf
 from batch_dataloader import Loader
 from model import LightGCN
 
-def inference(rating, uid, top_k=100) -> list:
+from functools import wraps
+
+def load_data_model(func):
+    @wraps(func) 
+    def wrapper(config, root_path, *args, **kwargs):
+        dataset = Loader(config=config, path=os.path.join(root_path, config.path.DATA))
+        model = LightGCN(config, dataset)
+        checkpoint = torch.load(os.path.join(root_path, config.path.FILE, 'best_model.pth'), map_location=torch.device('cuda'))
+        model.load_state_dict(checkpoint)
+        return func(dataset, model, *args, **kwargs)
+
+    return wrapper
+
+@load_data_model
+def inference(dataset, model, uid, top_k=100) -> list:
     '''
     user_embs, item_embs: 학습된 유저와 아이템 임베딩 (emb_dim:64)
     uid: 유저 아이디 (int)
     '''
+    user_embs, item_embs = model.embedding_user.weight, model.embedding_item.weight
+    print(f"graph shape is users: {user_embs.shape}, items: {item_embs.shape}")
+    ratings = torch.matmul(user_embs, item_embs.T)
+
     _, indices = torch.topk(ratings[uid], top_k)
 
     return indices
 
+def get_item_node(model, iid) -> torch.embedding:
+    item_nodes = model.embedding_item.weight
+    return item_nodes.detach()[iid]
+
+def get_all_user_node(dataset, model) -> list:
+    user_nodes = model.embedding_user.weight
+    return user_nodes
+
 if __name__=='__main__':
     config = OmegaConf.load('config.yaml')
     print(f"config: {OmegaConf.to_yaml(config)}")
-
     ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
-    dataset = Loader(config=config, path=os.path.join(ROOT_PATH,config.path.DATA))
-    model = LightGCN(config, dataset)
-    checkpoint = torch.load(os.path.join(ROOT_PATH, config.path.FILE, 'best_model.pth'), map_location=torch.device('cuda'))
-    model.load_state_dict(checkpoint)
     
-    user_embs, item_embs = model.embedding_user.weight, model.embedding_item.weight
-    print(user_embs.shape, item_embs.shape)
-    ratings = torch.matmul(user_embs, item_embs.T)
-    
-    for uid in range(len(user_embs)):
-        print(inference(ratings, uid))
+    for uid in range(config.num_users):
+        print(inference(uid))
