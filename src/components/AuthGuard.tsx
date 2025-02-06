22c66ebd98a64265b687ab5ebdbc9ca7f79120fd
jjjaeunn <jjjaeunn@gmail.com>
2025-02-06 10:27:30 +0900

    [KAN-64] feat: 유저 임베딩 유무에 따라 home, onboarding 설정

--- src/components/AuthGuard.tsx
+++ src/components/AuthGuard.tsx
@@ -1,7 +1,36 @@
-import { ReactNode } from "react";
+import { useSuspenseQuery } from "@suspensive/react-query";
+import { ReactNode, Suspense } from "react";
+import { Navigate } from "react-router-dom";
+import { userEmbeddingQuery } from "~/remotes";
 import useAuthorize from "~/utils/useAuthorize";
+import { FullScreenLoader } from "./FullScreenLoader";
 
 export const AuthGuard = ({ children }: { children: ReactNode }) => {
   const { id } = useAuthorize();
-  return id ? <>{children}</> : <>유저정보 가져오는 중</>;
+
+  if (location.pathname === "/") {
+    return <>{children}</>;
+  }
+  return id ? (
+    <Suspense fallback={<FullScreenLoader />}>
+      <AssertEmbedded id={id}>{children}</AssertEmbedded>
+    </Suspense>
+  ) : (
+    <FullScreenLoader />
+  );
+};
+
+const AssertEmbedded = ({
+  id,
+  children,
+}: {
+  id: number;
+  children: ReactNode;
+}) => {
+  const { data } = useSuspenseQuery(userEmbeddingQuery(id));
+
+  if (data.exist === false && location.pathname !== "/onboarding") {
+    return <Navigate to={"/onboarding"} />;
+  }
+  return <>{children}</>;
 };
